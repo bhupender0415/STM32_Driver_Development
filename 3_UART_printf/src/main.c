e80365a0d1e23c2726d4f69c9ab9c2d33d47597a
Bhupender Singh <bhupender0415@gmail.com>
2023-11-25 12:47:32 +0530

    Fixed-printf retarget uart

--- 3_UART_printf/src/main.c
+++ 3_UART_printf/src/main.c
@@ -1,39 +1,37 @@
 #include "stm32f4xx_hal.h"
 #include <stdio.h>
 
-/*
-PA2 -> TX
-PA3 -> RX
-
-*/
-
 UART_HandleTypeDef huart2;
 void uart_init(void);
 
-// Retargetting printf with uart
-
-// not working
-int __io_putchar(int ch)
+int _write(int file, char *ptr, int len)
 {
-    HAL_UART_Transmit(&huart2, (uint8_t *)&ch, 1, 100);
-    return ch;
+    // Redirect stdout to UART
+    HAL_UART_Transmit(&huart2, (uint8_t *)ptr, len, HAL_MAX_DELAY);
+    return len;
 }
 
+void stdout_init(void)
+{
+    // Initialize standard output stream
+    setvbuf(stdout, NULL, _IONBF, 0);
+}
 
 int main()
 {
     HAL_Init();
     uart_init();
+    
     while(1)
     {
-        printf("This is Printed using printf\r\n"); //not working
+        printf("This is Printed using printf \n\r");
         HAL_Delay(1000);
     }
 }
 
 void SysTick_Handler(void)
 {
-  HAL_IncTick();
+    HAL_IncTick();
 }
 
 void uart_init()
@@ -49,7 +47,7 @@ void uart_init()
     GPIO_InitStruct.Pull = GPIO_NOPULL;
     GPIO_InitStruct.Speed = GPIO_SPEED_FREQ_VERY_HIGH;
     GPIO_InitStruct.Alternate = GPIO_AF7_USART2;
-    HAL_GPIO_Init (GPIOA, &GPIO_InitStruct);   
+    HAL_GPIO_Init(GPIOA, &GPIO_InitStruct);   
 
     // configure usart module
     huart2.Instance = USART2;
@@ -57,9 +55,9 @@ void uart_init()
     huart2.Init.WordLength = UART_WORDLENGTH_8B;
     huart2.Init.StopBits = UART_STOPBITS_1;
     huart2.Init.Parity = UART_PARITY_NONE;
-    huart2.Init.Mode = UART_MODE_TX;
+    huart2.Init.Mode = UART_MODE_TX_RX;
     huart2.Init.HwFlowCtl = UART_HWCONTROL_NONE;
     huart2.Init.OverSampling = UART_OVERSAMPLING_16;
     HAL_UART_Init(&huart2);
-
 }
+   
