d517679112c2a87dc47842cc450e86a914a8f51b
Bhupender Singh <bhupender0415@gmail.com>
2023-11-25 10:12:42 +0530

    Added documentation to the function

--- 4_GPIO_Interrupt_EXTI/src/main.c
+++ 4_GPIO_Interrupt_EXTI/src/main.c
@@ -19,13 +19,29 @@ int main()
     }
 }
 
+
+
+
+/**
+    * @brief  SysTick Handler.
+    *         This function is called when the SysTick interrupt occurs.
+    * @param  None
+    * @retval None
+    */
 void SysTick_Handler(void)
 {
-  HAL_IncTick();
+    HAL_IncTick();
 }
 
 
 
+/**
+ * @brief Initializes the GPIO interrupt for PC13 button and configures the output LED.
+ * 
+ * This function enables the clock for GPIO ports A and C, configures the button pin as an interrupt
+ * with rising edge trigger, and configures the LED pin as an output. It also sets the priority and enables
+ * the EXTI interrupt for PC13 button.
+ */
 void gpio_pc13_interrupt_init(void)
 {
 
@@ -55,13 +71,24 @@ void gpio_pc13_interrupt_init(void)
     HAL_NVIC_EnableIRQ(EXTI15_10_IRQn);
 }
 
-// do task when interrupt occurs
+
+/**
+ * @brief Callback function for GPIO EXTI interrupt.
+ * @param GPIO_Pin: The pin number that triggered the interrupt.
+ * @retval None
+ */
 void HAL_GPIO_EXTI_Callback(uint16_t GPIO_Pin)
 {
     HAL_GPIO_TogglePin(LED_GPIO_PORT, LED_PIN);
 }
 
 
+/**
+    * @brief  EXTI line 15 to 10 interrupt handler.
+    *         This function is called when an interrupt event occurs on the EXTI line 15 to 10.
+    * @param  None
+    * @retval None
+    */
 void EXTI15_10_IRQHandler(void)
 {
     HAL_GPIO_EXTI_IRQHandler(BTN_PIN);
