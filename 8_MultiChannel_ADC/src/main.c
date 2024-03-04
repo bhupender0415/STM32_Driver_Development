d7c4601547024aea2e4c481fab90b0b84940e6ae
Bhupender Singh <bhupender0415@gmail.com>
2024-03-04 23:15:25 +0530

    added non blocking delay to toggle LED

--- 8_MultiChannel_ADC/src/main.c
+++ 8_MultiChannel_ADC/src/main.c
@@ -7,6 +7,9 @@
 ADC_HandleTypeDef hadc1;
 uint32_t adc_value[6] = {0};
 uint16_t ind = 0;
+uint32_t previous = 0, current = 0, ledInterval = 0;
+
+void toggleLED();
 
 int main()
 {
@@ -17,8 +20,24 @@ int main()
 
   while(1)
   {
+    current = HAL_GetTick();
+
+    // print ADC buffer to serial monitor
+    if(current-previous >= 1000){
     printf("ADC value: %ld %ld %ld %ld %ld %ld\n", adc_value[0], adc_value[1], adc_value[2], adc_value[3], adc_value[4], adc_value[5]);
-    HAL_Delay(2000);
+    previous = current;
+    }
+
+    // toggle LED
+    if(current-ledInterval >= 500){
+      toggleLED();
+      ledInterval = current;
+    }
+
+
+    // blocking delay
+    //printf("ADC value: %ld %ld %ld %ld %ld %ld\n", adc_value[0], adc_value[1], adc_value[2], adc_value[3], adc_value[4], adc_value[5]);
+    //HAL_Delay(2000);
 
 
   }
@@ -42,5 +61,14 @@ void HAL_ADC_ConvCpltCallback(ADC_HandleTypeDef* hadc)
       ind = 0;
     }
     HAL_ADC_Start_IT(&hadc1);   // start ADC interrupt
+  }
+}
+
+void toggleLED()
+{
+  if(adc_value[0] > 3000)
+  {
+    HAL_GPIO_TogglePin(GPIOA, GPIO_PIN_5);
+    printf("Inside toggle LED function\n");
   }
 }
