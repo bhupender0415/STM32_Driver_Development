68c41049b779e846eeb36b84820e09cd5ba28477
Bhupender Singh <bhupender0415@gmail.com>
2024-03-07 22:34:14 +0530

    add header file for gpio and timer

---
+++ 9_Timer/include/gpio.h
@@ -0,0 +1,7 @@
+#ifndef GPIO_H
+#define GPIO_H
+#include "stm32f4xx_hal.h"
+
+void PA5_init(void);
+
+#endif // GPIO_H
---
+++ 9_Timer/include/timer.h
@@ -0,0 +1,11 @@
+#ifndef TIMER_H
+#define TIMER_H
+
+#include "stm32f4xx_hal.h"
+
+/* Function prototypes */
+void TIM2_Init(void);
+void TIM2_IRQHandler(void);
+
+
+#endif // TIMER_H
